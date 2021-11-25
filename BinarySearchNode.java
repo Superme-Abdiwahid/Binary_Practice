481512c9aac07a4b63cfe1b4a3a81cffba6d4bc5
Superme-Abdiwahid <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-11-25 14:24:58 -0800

    Update Demo

--- BinarySearchNode.java
+++ BinarySearchNode.java
@@ -1,16 +1,17 @@
-// Class that represents a single node in the tree.
+// Class that represents a single BinarySearchNode in the tree.
 public class BinarySearchNode {
     public String data;
     public BinarySearchNode left;
     public BinarySearchNode right;
 
-    // Constructs a leaf node with the given data. As a string
+    // Constructs a leaf node starts out null with the given data. As a string
     public BinarySearchNode(String data) {
         this(data, null, null);
     }
 
     // Constructs a leaf or branch node with the given data and links.
-    // to perform a binary search later on 
+    // to perform a binary search later on. Used to also create nodes of a 
+    //intital tree
     public BinarySearchNode(String data, BinarySearchNode left, BinarySearchNode right) {
         this.data = data;
         this.left = left;
